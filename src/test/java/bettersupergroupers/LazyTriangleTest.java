82e997ba523704f8a03abd2d2c99bab2f07739e2
dhasler <haslerdavidw@gmail.com>
2017-05-31 15:57:12 -0400

    3 test cases

--- src/test/java/bettersupergroupers/LazyTriangleTest.java
+++ src/test/java/bettersupergroupers/LazyTriangleTest.java
@@ -1,13 +1,38 @@
-import org.junit.test
-import static org.junit.Assert.*
+import org.junit.Test;
+import static org.junit.Assert.*;
 
-import bettersupergroupers.LazyTriangle
+import bettersupergroupers.LazyTriangle;
 
 public class LazyTriangleTest {
 
   @Test
   public void testLazy0(){
-    
+    assertEquals(LazyTriangle.lazySequence(0), 1);
   }
 
+  @Test
+  public void testTri0(){
+    assertEquals(LazyTriangle.triangleSequence(0), 0);
+  }
+
+  @Test
+  public void testLazy3(){
+    assertEquals(LazyTriangle.lazySequence(3), 7);
+  }
+
+  @Test
+  public void testTri3(){
+    assertEquals(LazyTriangle.triangleSequence(3), 6);
+  }
+
+
+  @Test
+  public void testLazy7(){
+    assertEquals(LazyTriangle.lazySequence(7), 29);
+  }
+
+  @Test
+  public void testTri7(){
+    assertEquals(LazyTriangle.triangleSequence(7), 28);
+  }
 }
