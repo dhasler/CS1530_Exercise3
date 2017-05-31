427d19c2158879f167bd9c6f567fe206e758fe8b
dhasler <haslerdavidw@gmail.com>
2017-05-31 15:34:36 -0400

    adding test units

---
+++ src/test/java/bettersupergroupers/LazyTriangleTest.java
@@ -0,0 +1,13 @@
+import org.junit.test
+import static org.junit.Assert.*
+
+import bettersupergroupers.LazyTriangle
+
+public class LazyTriangleTest {
+
+  @Test
+  public void testLazy0(){
+    
+  }
+
+}
